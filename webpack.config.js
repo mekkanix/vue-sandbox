76eda1ae0fc59688c14b7a4f846b139c226e2b62
polo <paulguzdariviere@gmail.com>
2022-01-17 18:20:53 +0100

    Rework wp config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,146 +1,15 @@
-const path = require('path')
-const autoprefixer = require('autoprefixer')
-const TerserPlugin = require('terser-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts")
-const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const NodemonPlugin = require('nodemon-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+/**
+ * Webpack configuration entrypoint.
+ * This script will simply select the right configuration to use
+ * depending on the given `mode` option.
+ */
 
-module.exports = (env, argv) => {
+const configs = {
+  dev: require('./webpack.dev.js'),
+  production: require('./webpack.production.js'),
+}
 
+module.exports = (_, argv) => {
   const mode = argv.mode && argv.mode === 'production' ? argv.mode : 'development'
-  const styleFilename = mode === 'production' ? 'vue-sandbox.min.css' : 'vue-sandbox.css'
-
-  const config = {
-    // Build configuration (local)
-    mode: mode,
-    entry: {
-      'vue-sandbox': './app/main.js',
-      'vue-sandbox.min': './app/main.js',
-    },
-    output: {
-      library: 'VS',
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      path: path.resolve(__dirname, './app/assets/dist/'),
-      publicPath: '/',
-      filename: '[name].js',
-    },
-    module: {
-      rules: [
-        // Pkg-related rules (internal)
-        {
-          test: /\.js$/i,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-              }
-            },
-          ]
-        },
-        {
-          test: /\.vue$/i,
-          loader: 'vue-loader',
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          exclude: [
-            path.join(__dirname, 'public/components/')
-          ],
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sassOptions: {
-                  indentedSyntax: true,
-                  includePaths: [
-                    path.resolve(__dirname, 'node_modules'),
-                    path.join(__dirname, 'app/assets/sass'),
-                  ]
-                },
-              },
-            },
-          ]
-        },
-        // User-provided components-related rules
-        {
-          test: /\.s[ac]ss$/i,
-          exclude: [
-            path.join(__dirname, 'app/')
-          ],
-          use: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sassOptions: {
-                  indentedSyntax: true,
-                  includePaths: [
-                    path.resolve(__dirname, 'node_modules'),
-                    path.join(__dirname, 'app/assets/sass'),
-                  ]
-                },
-              },
-            },
-          ]
-        }
-      ]
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'app/'),
-        '@public': path.resolve(__dirname, 'public/'),
-      }
-    },
-    plugins: [
-      new VueLoaderPlugin(),
-      new MiniCssExtractPlugin({
-        filename: styleFilename,
-      }),
-      new RemoveEmptyScriptsPlugin(),
-    ],
-  }
-
-  if (mode === 'production') {
-    config.optimization = {
-      minimizer: [
-        new TerserPlugin(),
-        new CSSMinimizerPlugin(),
-      ]
-    }
-    config.plugins.push(autoprefixer)
-  }
-
-  if (mode === 'development') {
-    const analyzerPluginConfig = new BundleAnalyzerPlugin({
-      analyzerMode: 'server',
-      analyzerPort: 9001,
-      generateStatsFile: true,
-      openAnalyzer: false,
-    })
-    const nodemonPluginConfig = new NodemonPlugin({
-      script: './app/server.js',
-      watch: path.resolve('./app/server.js'),
-      env: {
-        VS_ENV: 'development',
-      }
-    })
-    config.plugins.push(analyzerPluginConfig)
-    config.plugins.push(nodemonPluginConfig)
-  }
-
-  return config
-
+  return mode === 'production' ? configs.production : configs.dev
 }
