db5bcffd97f86e040b08f19446cb915a1fcc40b5
polo <paulguzdariviere@gmail.com>
2022-01-22 16:48:53 +0100

    Remove useless comment

--- app/server/config.js
+++ app/server/config.js
@@ -1,6 +1,5 @@
 // TODO: Remove this CLI args parsing from server entrypoint, by using
 // a dedicated binary script to manage on-the-fly VS setup.
-// const argv = yargsParser(process.argv.slice(2))
 const yargsParser = require('yargs-parser')
 const argv = yargsParser(process.argv.slice(2))
 const assetsPrefix = argv.assetsPrefix || null
