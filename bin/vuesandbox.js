e9b7082ca211d362e4a06e21e711844bc1e2fb70
mekkanix <paulguzdariviere@gmail.com>
2022-01-23 03:45:29 +0100

    Add log for binary operations

--- bin/vuesandbox.js
+++ bin/vuesandbox.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
+require('module-alias/register')
+const pkg = require('@root/package.json')
 const { Command } = require('commander')
 const program = new Command()
 
+const VS_ENV = process.env.VS_ENV
+console.log(`\r\n[VueSandbox - v${pkg.version}]`)
+console.log(`> Mode: ${VS_ENV}\r\n`)
+
 program
   .command('start')
-  .command('description', 'test command')
+  .description('Starts the VueSandbox server daemon.')
   .option('-p, --assetsprefix [prefix]', 'test command option', '')
   .action((args) => {
+    console.log(`[VueSandbox] Starting server...`)
     console.log(args);
   })
 
