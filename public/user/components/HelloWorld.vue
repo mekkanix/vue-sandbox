2f81369caef32bdb7f02ea9e667faa0f6561874f
polo <paulguzdariviere@gmail.com>
2022-03-12 11:15:09 +0100

    Restructure public/

---
+++ public/user/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: 'By editing this you can see the component in action!',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  text-align: center;
+}
+p {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ public/user/components/someFolder/HelloWorld2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <img src="/sitecontents/assets/toto.jpeg" />
+    <img src="/sitecontents/assets/path/to/toto.jpeg" />
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld2',
+  props: {
+    msg: {
+      type: String,
+      default: 'By editing this you can see the component in action!',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  text-align: center;
+}
+p {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
